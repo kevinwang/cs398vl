ff8d0cb0ee4b9ced09593fca51431fa8c0dfd9ce
Kevin Wang <kevin@kevinwang.com>
2014-02-11 12:19:24 -0600

    Simplify text processing code.

--- mp1/mp1.py
+++ mp1/mp1.py
@@ -7,9 +7,8 @@ import json
 
 wordlists = PlaintextCorpusReader('', '../corpora/ofk_ch[1234]\.txt')
 stopwords_set = set(stopwords.words('english'))
-all_words = []
+all_words = set()
 freqs = []
-ch_lens = []
 
 for i in xrange(1, 5):
     words = wordlists.words('../corpora/ofk_ch' + str(i) + '.txt')
@@ -17,21 +16,15 @@ for i in xrange(1, 5):
     words = [w for w in words if w.isalpha()]
     words = [w for w in words if not w in stopwords_set]
     words = [PorterStemmer().stem(w) for w in words]
-    all_words += words
-    ch_lens.append(len(words))
+    all_words.update(words)
     freq = FreqDist(words)
     freqs.append(freq)
     print freq
 
-all_freq = FreqDist(all_words)
-
 slopes = []
 x = [1, 2, 3, 4]
-for w in all_freq:
-    y = [float(freqs[0][w])/ch_lens[0],
-            float(freqs[1][w])/ch_lens[1],
-            float(freqs[2][w])/ch_lens[2],
-            float(freqs[3][w])/ch_lens[3]]
+for w in all_words:
+    y = [freqs[0].freq(w), freqs[1].freq(w), freqs[2].freq(w), freqs[3].freq(w)]
     slope = linregress(x, y)[0]
     slopes.append({'word': w, 'slope': slope})
 
@@ -39,10 +32,10 @@ slopes_sorted = sorted(slopes, key=itemgetter('slope'), reverse=True)
 
 out_data = slopes_sorted[:20] + slopes_sorted[-20:]
 for w in out_data:
-    w['freqs'] = [{'ch': 1, 'freq': float(freqs[0][w['word']])/ch_lens[0]},
-            {'ch': 2, 'freq': float(freqs[1][w['word']])/ch_lens[1]},
-            {'ch': 3, 'freq': float(freqs[2][w['word']])/ch_lens[2]},
-            {'ch': 4, 'freq': float(freqs[3][w['word']])/ch_lens[3]}]
+    w['freqs'] = [{'ch': 1, 'freq': freqs[0].freq(w['word'])},
+            {'ch': 2, 'freq': freqs[1].freq(w['word'])},
+            {'ch': 3, 'freq': freqs[2].freq(w['word'])},
+            {'ch': 4, 'freq': freqs[3].freq(w['word'])}]
 
 with open('data.json','w') as outfile:
      json.dump(out_data, outfile, sort_keys=True, indent=4, ensure_ascii=False)
